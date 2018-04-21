d0eff0d73a80140db40a03a19051cf2191ed95dd
Aaron Nemoyten <aaron@nemo10.net>
2018-04-20 22:03:59 -0700

    More changes, movement code, stew, jumping

--- UnityProject/LD41-Nemoyten/Assets/FirstPersonController.cs
+++ UnityProject/LD41-Nemoyten/Assets/FirstPersonController.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour {
 
+	public GameObject feets;
+	public LayerMask groundCollision;
+
 	Rigidbody rb;
+
+	float speed = 5f;
+	float jumpVelocity = 5f;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 	
 	void Update () {
-		float move = Input.GetAxisRaw("Vertical");
-		float strafe = Input.GetAxisRaw("Horizontal");
+		float move = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+		float strafe = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
 
-		rb.AddForce(move, 0, strafe);
+		rb.transform.Translate(new Vector3(strafe, 0, move));
+
+		if (Input.GetButtonDown("Jump") && Physics.Raycast(feets.transform.position, Vector3.down, .2f, groundCollision)){
+			Debug.Log("Jumping");
+			rb.AddForce(new Vector3(0, jumpVelocity, 0), ForceMode.Impulse);
+		}
 	}
 }
