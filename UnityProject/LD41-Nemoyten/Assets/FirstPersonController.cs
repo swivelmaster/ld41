9fc6d6988ad7294e8d7c6b6f2382d5f08fd91130
Aaron Nemoyten <aaron@nemo10.net>
2018-04-20 21:19:25 -0700

    Second commit - some basic stuff!

---
+++ UnityProject/LD41-Nemoyten/Assets/FirstPersonController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		float move = Input.GetAxisRaw("Vertical");
+		float strafe = Input.GetAxisRaw("Horizontal");
+
+		rb.AddForce(move, 0, strafe);
+	}
+}
