c538cf5fbd91b7694e6bd72a39af6c754f415f25
Aaron Nemoyten <aaron@nemo10.net>
2018-04-20 22:18:06 -0700

    Adding /Temp to ignore (duh), also reorganizing some folders, adding DEATH

---
+++ UnityProject/LD41-Nemoyten/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public GameObject feets;
+	public LayerMask groundCollision;
+
+	Rigidbody rb;
+
+	float speed = 5f;
+	float jumpVelocity = 5f;
+
+	public Text deathText;
+
+	bool alive = true;
+
+	public GameObject stew;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		if (!alive){
+			return;
+		}
+
+		float move = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+		float strafe = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+
+		rb.transform.Translate(new Vector3(strafe, 0, move));
+
+		if (Input.GetButtonDown("Jump") && Physics.Raycast(feets.transform.position, Vector3.down, .2f, groundCollision)){
+			Debug.Log("Jumping");
+			rb.AddForce(new Vector3(0, jumpVelocity, 0), ForceMode.Impulse);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject == stew){
+			alive = false;
+			deathText.enabled = true;
+			deathText.text = "GAME OVER: YOU WERE NOT ONE OF THE INGREDIENTS";
+		}
+	}
+}
